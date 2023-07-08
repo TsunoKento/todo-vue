bc37f2bf042ceb4e553c6595e9d0dedc3d749fd3
TsunoKento <jinyejianren@gmail.com>
2023-07-08 16:53:48 +0900

    一覧の表示機能の作成

---
+++ src/src/components/UncompletedTodoList.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const todoList = ref([
+  { id: 1, title: "aaa" },
+  { id: 2, title: "bbb" },
+  { id: 3, title: "ccc" },
+]);
+</script>
+
+<template>
+  <h2>未完了</h2>
+  <v-card class="mx-auto" max-width="300">
+    <v-list :items="todoList" item-value="id" />
+  </v-card>
+</template>
--- src/src/views/TodoView.vue
+++ src/src/views/TodoView.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import UncompletedTodoList from "../components/UncompletedTodoList.vue";
+</script>
+
 <template>
   <h1>TODO</h1>
+  <UncompletedTodoList />
 </template>
