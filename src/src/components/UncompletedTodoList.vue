f6b71529feb7abc1af3bc6608f889dd5b3ad73f0
Tsuno Kento <jinyejianren@gmail.com>
2023-07-26 06:50:50 +0900

    feature: todoリストの検索結果をAPIから取得できるように変更
    TODOがないときは表示しないように変更

--- src/src/components/UncompletedTodoList.vue
+++ src/src/components/UncompletedTodoList.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
+import apiClient from "@/http-common";
+import { Ref } from "vue";
+import { onMounted } from "vue";
 import { ref } from "vue";
 
-const todoList = ref([
-  { id: 1, title: "aaa" },
-  { id: 2, title: "bbb" },
-  { id: 3, title: "ccc" },
-]);
+interface Todo {
+  id: number;
+  title: string;
+}
+
+const todoList: Ref<Todo[] | undefined> = ref();
+
+onMounted(async () => {
+  const response = await apiClient.get("/api/todos");
+  todoList.value = response.data.data;
+});
 </script>
 
 <template>
   <h2>未完了</h2>
-  <v-card class="mx-auto">
+  <v-card class="mx-auto" v-if="todoList">
     <v-list :items="todoList" item-value="id" />
   </v-card>
 </template>
